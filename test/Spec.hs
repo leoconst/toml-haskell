f995f334e747097b084f049aa3624065d45798cc
leoconst <35425444+leoconst@users.noreply.github.com>
2022-02-19 20:26:08 +0000

    Add testcase for key-value pair

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,17 @@
-import Test.Hspec
+import qualified Data.Map as Map
+
 import qualified Toml
 
-import Data.Either (isRight)
-import qualified Data.Map as Map
+import Test.Hspec
 
 
 main :: IO ()
 main = hspec $ do
+
   describe "Toml.decode" $ do
+
     it "returns empty table for empty source" $
       Toml.decode "" `shouldBe` Right Map.empty
+
+    it "decodes a simple key-value pair" $
+      Toml.decode "key = 'value'" `shouldBe` Right (Map.fromList [("key", Toml.String "value")])
